f496f91124b345996b61410dc3cb39d2892cac3e
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-20 19:30:39 +0300

    Add database

---
+++ src/Pakohuone/Data/PakohuoneContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pakohuone.Data
+{
+    public class PakohuoneContext : DbContext
+    {
+        public PakohuoneContext(DbContextOptions<PakohuoneContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+    }
+}
--- src/Pakohuone/Startup.cs
+++ src/Pakohuone/Startup.cs
@@ -2,14 +2,25 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Pakohuone.Data;
 
 namespace Pakohuone
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+        
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PakohuoneContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("Pakohuone")));
+
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
