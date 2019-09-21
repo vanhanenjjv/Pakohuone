c9554afea29c1a14c106826c2c28743bf30cf31d
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-22 01:06:31 +0300

    Add Admin layout

---
+++ src/Pakohuone/wwwroot/js/admin/layout.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(() => {
+  $('#sidebar-toggle').click(toggleSidebar);
+});
+
+const toggleSidebar = () => {
+  $('#wrapper').toggleClass('toggled');
+  $('#sidebar-toggle').toggleClass('active');
+}
