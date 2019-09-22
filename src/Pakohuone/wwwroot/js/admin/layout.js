95ebc3e33b8f34f5c697b7cf0a880ea88a542aae
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-22 17:40:16 +0300

    Move table initialization into it's own script

--- src/Pakohuone/wwwroot/js/admin/layout.js
+++ src/Pakohuone/wwwroot/js/admin/layout.js
@@ -1,25 +1,8 @@
 ﻿$(document).ready(() => {
   $('#sidebar-toggle').click(toggleSidebar);
-  initializeDataTables();
 });
 
 const toggleSidebar = () => {
   $('#wrapper').toggleClass('toggled');
   $('#sidebar-toggle').toggleClass('active');
-}
-
-const initializeDataTables = () => {
-  const tables = document.getElementsByClassName('ph-table');
-
-  for (const table of tables) {
-    $(table).DataTable({
-      'language': {
-        "lengthMenu": "Showing _MENU_ items per page",
-        "zeroRecords": "No items found",
-        "info": "Showing page _PAGE_ of _PAGES_",
-        "infoEmpty": "Nothing matches the filter",
-        "infoFiltered": "(filtered from _MAX_ total items)"
-      }
-    });
-  }
 }
---
+++ src/Pakohuone/wwwroot/js/admin/table.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(() => {
+  initializeDataTables();
+});
+
+const initializeDataTables = () => {
+  const tables = document.getElementsByClassName('ph-table');
+
+  for (const table of tables) {
+    $(table).DataTable({
+      'language': {
+        "lengthMenu": "Showing _MENU_ items per page",
+        "zeroRecords": "No items found",
+        "info": "Showing page _PAGE_ of _PAGES_",
+        "infoEmpty": "Nothing matches the filter",
+        "infoFiltered": "(filtered from _MAX_ total items)"
+      }
+    });
+  }
+}
