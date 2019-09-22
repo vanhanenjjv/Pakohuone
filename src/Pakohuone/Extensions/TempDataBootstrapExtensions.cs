2744a74f2fb72d1051edc3e9c1252677b77356e4
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-22 18:40:18 +0300

    Add bootstrap alerts

---
+++ src/Pakohuone/Extensions/TempDataBootstrapExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Pakohuone.Models.Bootstrap;
+
+namespace Pakohuone.Extensions
+{
+    public static class TempDataBootstrapExtensions
+    {
+        public static void PutAlert(this ITempDataDictionary tempData, Alert alert)
+        {
+            tempData.Put("Alert", alert);
+        }
+
+        public static Alert GetAlert(this ITempDataDictionary tempData)
+        {
+            return tempData.Get<Alert>("Alert");
+        }
+    }
+}
---
+++ src/Pakohuone/Models/Bootstrap/Alert.cs
@@ -0,0 +1,33 @@
+﻿// https://getbootstrap.com/docs/4.3/components/alerts/
+
+namespace Pakohuone.Models.Bootstrap
+{
+    public class Alert
+    {
+        public Alert()
+        {
+        }
+
+        public Alert(string text) =>
+            (Text, Type) = (text, AlertType.Primary);
+
+        public Alert(string text, AlertType type) =>
+            (Text, Type) = (text, type);
+
+        public string Text { get; set; }
+
+        public AlertType Type { get; set; }
+    }
+
+    public enum AlertType
+    {
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark
+    }
+}
