b994c9459a4e584695dcb79753e040f59e755c98
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-22 19:34:48 +0300

    Add helper class for very useful generic messages

---
+++ src/Pakohuone/Common/GenericMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Pakohuone.Common
+{
+    public static class GenericMessage
+    {
+        public static string Error => 
+            "OOPSIE WOOPSIE!! OwO We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!";
+    }
+}
