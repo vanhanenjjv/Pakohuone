d681a54e3cf1b4f81f978f26064e8c6d3b506a1b
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-22 18:48:52 +0300

    Increase upload's size limit to 500 MB

---
+++ src/Pakohuone/Areas/Admin/AdminHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Pakohuone.Areas.Admin.AdminHostingStartup))]
+namespace Pakohuone.Areas.Admin
+{
+    public class AdminHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.Configure<FormOptions>(options =>
+                {
+                    options.ValueLengthLimit = 526628572;
+                    options.MultipartBodyLengthLimit = 526628572;
+                });
+            });
+        }
+    }
+}
