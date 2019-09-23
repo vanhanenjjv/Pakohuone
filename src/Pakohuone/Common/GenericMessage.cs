dbe30e781bf5e779587ced6282fabf2040f036e0
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-23 15:41:57 +0300

    Split the message into two lines

--- src/Pakohuone/Common/GenericMessage.cs
+++ src/Pakohuone/Common/GenericMessage.cs
@@ -3,6 +3,7 @@
     public static class GenericMessage
     {
         public static string Error => 
-            "OOPSIE WOOPSIE!! OwO We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!";
+            "OOPSIE WOOPSIE!! OwO We made a fucky wucky!! A wittle fucko boingo! " +
+            "The code monkeys at our headquarters are working VEWY HAWD to fix this!";
     }
 }
