eef85e9a7ea0d063f80795c994362288437b8e20
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-23 21:53:41 +0300

    Add generic message for saving changes successfully

--- src/Pakohuone/Common/GenericMessage.cs
+++ src/Pakohuone/Common/GenericMessage.cs
@@ -5,5 +5,7 @@
         public static string Error => 
             "OOPSIE WOOPSIE!! OwO We made a fucky wucky!! A wittle fucko boingo! " +
             "The code monkeys at our headquarters are working VEWY HAWD to fix this!";
+
+        public static string Save => "Saved changes.";
     }
 }
